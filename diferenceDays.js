6a24538d03fbb3ffdc1ad85864a9d619da825109
gigakhrz <kharazishvilig13@gmail.com>
2024-03-13 21:52:20 +0400

    add some algoritm

---
+++ diferenceDays.js
@@ -0,0 +1,10 @@
+// Create a function that takes two dates and returns the number of days
+// between the first and second date.
+
+const getDays = (date1, date2) => {
+  diff = date1.getTime() - date2.getTime();
+
+  const daysDiff = Math.ceil(diff / (1000 * 3600 * 24));
+
+  return daysDiff;
+};
